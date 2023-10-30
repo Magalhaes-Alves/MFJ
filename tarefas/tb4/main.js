89e462f98f4ca6b13f4db318c625e10cb848c582
Gabriel Magalhaes <magalhaesgabriel124@gmail.com>
2023-10-30 16:36:11 -0300

    traalho

--- tarefas/tb4/main.js
+++ tarefas/tb4/main.js
@@ -1,10 +1,34 @@
+function isVectorNull(v1){
+  return v1.mag()==0
+}
 
+function to_degree(r){
+  
+  return (180/PI)*r
+}
 
-function angle_vector_2d(v1,v2){
+function cos_vector_2d(v1,v2){
+  
+  if (isVectorNull(v1) || isVectorNull(v2)){
+    console.log("Um dos vetores é nulo.")
+    return
+  }
+  
+  return degrees(acos(p5.Vector.dot(v1,v2)/(v1.mag()*v2.mag())))
+  
+}
 
-  return acos(p5.Vector.dot(v1,v2)/(v1.mag()*v2.mag()))
+function sen_vector_2d(v1,v2,is3d){
 
-
+  if (isVectorNull(v1) || isVectorNull(v2)){
+    console.log("Um dos vetores é nulo.")
+    return
+  }
+   
+  let ang = p5.Vector.cross(v1,v2).mag()
+  
+  return degrees(asin(ang/(v1.mag()*v2.mag())))
+    
 }
 
 
@@ -19,8 +43,7 @@ function draw() {
   v2 = createVector(100,300)
 
 
-  angle_vector_2d(v1,v2)
-
-
+  console.log(cos_vector_2d(v1,v2))
+  console.log(sen_vector_2d(v1,v2))
 
 }
